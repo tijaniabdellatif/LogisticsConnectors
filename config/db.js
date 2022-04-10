708d981d725bfffead9d02dc322df27975981102
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-04-10 03:40:58 +0200

    connecting to mongoose

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+/**
+ *  ConnectDB
+ * @function ConnectDB
+ * @param null
+ * @async connection to mongoose Database
+ */
+const connectDB = async () => {
+
+        try{
+            await mongoose.connect(db);
+            console.log('MongoDB connected ...');
+        }
+        catch(error){
+
+                console.log(error.message);
+                // Exit proccess with failure
+                process.exit(1);
+        }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const connectDB = require('./config/db');
 const app = express();
 const __PORT__ = process.env.PORT || 5000;
+
+/**
+ * Connecting DB
+ */
+
+connectDB();
+
+
 
 app.get('/',(req,res) => {
 
