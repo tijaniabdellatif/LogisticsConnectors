5c053add9862cf71090c9e147dd37cc834b8932b
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-04-10 05:06:43 +0200

    setup routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
+
 const app = express();
 const __PORT__ = process.env.PORT || 5000;
 
@@ -9,13 +10,18 @@ const __PORT__ = process.env.PORT || 5000;
  */
 
 connectDB();
-
-
-
 app.get('/',(req,res) => {
 
        res.send(`API Runing welcome ${process.env.APP_NAME}`);
 })
+/**
+ * define routes
+ */
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profiles',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
 app.listen(__PORT__,() => {
 
       console.log( `Server Started on ${__PORT__} welcome ${process.env.NAME}
