2b0b6cfd773527ead2ebd5debee55e8a833f586a
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-04-15 17:22:09 +0200

    token setup and verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 /**
  * @route  GET api/auth
  * @desc   Test route
  * @access Public
  */
-router.get('/',(req,res) => {
-     res.send('Auth Route');
+router.get('/',auth, async (req,res) => {
+     
+     try{
+
+          const user = await User.findById(req.user.id)
+          .select('-password');
+          res.json(user);
+
+
+     }catch(err){
+
+          console.log(err.message);
+          res.status(500).send({
+                   status:500,
+                   message:'Error server'
+          })
+
+     }
 })
 
 
