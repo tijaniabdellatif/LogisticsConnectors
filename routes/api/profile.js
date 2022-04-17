d7dcf08f88a7270af61c2de70e7f74bb8ed0281e
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-04-17 04:40:48 +0200

    profile getting with error

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 /**
- * @route  GET api/profile
- * @desc   Test route
- * @access Public
+ * @route get api/profile/me
+ * @description Get current user profile
+ * @access private {token based}
+ * @param String uri 
+ * @param Array validation
+ * @callback (request,response)
  */
-router.get('/',(req,res) => {
-     res.send('profile Route');
+router.get('/me',auth,async (req,res) => {
+     
+     try{
+             const profile  = await Profile.findOne({ user : req.user.id})
+             .populate('user',['name','avatar']);
+
+             if(!profile){
+
+                res.status(400).json({
+                        
+                        status:res.statusCode,
+                        msg:"there's no profile for this user"
+                }) 
+             }
+
+
+             res.json(profile);
+
+     }catch(e){
+
+           console.error(e.message);
+           res.status(500).send('Server Error')
+     }
 })
 
 
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/',(req,res) => {
  */
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profiles',require('./routes/api/profile'));
+app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
 
 app.listen(__PORT__,() => {
